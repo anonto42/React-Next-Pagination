5ba0ecd9bb6d57ebfc7920507127e47d3d101d60
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-05 14:40:35 +0600

    Source file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import AppLayout from '../Components/Layout/AppLayout'
+import { Box, Typography } from '@mui/material'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <Box height={"100%"}>
+    <Typography p={"2rem"} variant='h6' textAlign={"center"}>
+      Selected to chat the people you want
+    </Typography>
+  </Box>
 }
 
 export default AppLayout()(Home)
