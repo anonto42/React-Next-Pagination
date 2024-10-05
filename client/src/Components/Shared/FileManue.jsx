38dc425d71bd97abf4ed1e0367dd3a573273f518
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-05 14:42:20 +0600

    file manue

---
+++ client/src/Components/Shared/FileManue.jsx
@@ -0,0 +1,12 @@
+import { Menu } from '@mui/material'
+import React from 'react'
+
+const FileManue = ({anchorE1}) => {
+  return <Menu open anchorEl={anchorE1}>
+    <div style={{width:"10rem"}}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam necessitatibus illo mollitia tenetur a iste in commodi impedit est corporis rem architecto doloremque quasi minus deserunt magnam ipsum nam nihil iure nulla temporibus, molestiae distinctio reprehenderit praesentium. Beatae iure, nostrum, animi ex at sed iste quasi provident earum numquam hic.
+    </div>
+  </Menu>
+}
+
+export default FileManue
