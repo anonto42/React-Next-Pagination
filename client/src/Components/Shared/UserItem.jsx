d74254c2613a14818ef32e84727c5320230cd99f
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-07 16:18:42 +0600

    Border added

--- client/src/Components/Shared/UserItem.jsx
+++ client/src/Components/Shared/UserItem.jsx
@@ -3,14 +3,14 @@ import { Avatar, IconButton, ListItem, Stack, Typography } from '@mui/material'
 import React, { memo } from 'react'
 
 
-const UserItem = ({user,handler,handlerIsLoading,isAdded=false}) => {
+const UserItem = ({user,handler,handlerIsLoading,isAdded=false,styling={}}) => {
 
     const { name , _id , avatar } = user
 
   return (
     <ListItem>
-        <Stack direction={"row"} alignItems={"center"} spacing={"1rem"} width={"100%"}>
-            <Avatar src={avatar} />
+        <Stack direction={"row"} alignItems={"center"} spacing={"1rem"} width={"100%"}{...styling}>
+            <Avatar src={avatar? avatar : ""} />
             <Typography variant='body1' sx={{flexGlow:'1',display:"-webkit-box",WebkitLineClamp:1,WebkitBoxOrient:"vertical",overflow:"hidden",textOverflow:"ellipsis",width:"100%"}}>{name}</Typography>
             <IconButton onClick={()=>handler(_id) } disabled={handlerIsLoading} size='primary.main' sx={{bgcolor:"primary.main",color:"white","&:hover":{bgcolor:"primary.dark"}}}>
               {
