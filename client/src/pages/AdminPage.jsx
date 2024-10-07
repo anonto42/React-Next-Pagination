e4d188b176f55091e7d8648db97dfacf4ce604c5
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-08 00:25:34 +0600

    admin page makeing

---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLayout from '../Components/Layout/AdminLayout'
+
+const AdminPage = () => {
+  return (
+    <AdminLayout>
+      Dashbord
+    </AdminLayout>
+  )
+}
+
+export default AdminPage
