eb90092af9fdabbc65f05fa2293bfa6c99a6bd97
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-13 11:22:37 +0600

    db Connection created

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { route } from './src/routes/user.routes.js';
+import dotenv from "dotenv";
+import connectDB from './src/db/db.js';
 
 const app = express();
 
-app.use(express.json({}))
 
-app.use("/",route)
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api",route)
 
 app.listen( process.env.PORT , ()=> {
+    connectDB();
     console.log("your server listening the port : " , process.env.PORT)
 })
