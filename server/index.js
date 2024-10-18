2d46065b0dbda838685917c365507ce46465ed2c
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-18 12:38:03 +0600

    port config updated

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from './src/db/db.js';
 
 const app = express();
+const port = process.env.PORT || 3500;
 
 
 dotenv.config();
@@ -12,7 +13,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use("/api",route)
 
-app.listen( process.env.PORT , ()=> {
+app.listen( port , ()=> {
     connectDB();
-    console.log("your server listening the port : " , process.env.PORT)
+    console.log("your server listening the port : " , port )
 })
