a5357a15cc2c71b21b8fc3b5c091abb972d05e9b
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-18 12:38:21 +0600

    user creating

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,38 @@
+import { UserModel } from "../models/user.model.js"
 
 
 
 export const login = ( req , res ) => {
     res.json({"done":"Response was successfully sent"})
+}
+
+export const newUser = async ( req , res ) => { 
+    try {
+
+        const avatar = {
+            public_id: "test_public_id",
+            url: "test_url"
+        }
+
+        const userDataForCreate = {
+            name:"anonto",
+            userName:"anonto33",
+            password:"anontopass",
+            avatar
+        }
+        
+        const user = await UserModel.create( userDataForCreate )
+
+        res
+        .status(201)
+        .json(
+            {
+                user,
+                message: "User created successfully",
+            }
+        )
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
