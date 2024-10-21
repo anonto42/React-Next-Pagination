911e0c113ff59b8d7014cd84f296b1ff1ee55d57
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-22 04:06:59 +0600

    chat route added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { route } from './src/routes/user.routes.js';
 import dotenv from "dotenv";
 import connectDB from './src/db/db.js';
 import cookieParser from 'cookie-parser';
+import chatRouter from './src/routes/chat.routes.js';
 
 const app = express();
 const port = process.env.PORT || 3500;
@@ -13,7 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+// Routes
 app.use("/api",route)
+app.use("/api/chat",chatRouter)
 
 app.listen( port , ()=> {
     connectDB();
