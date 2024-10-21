0a237901e0f0a7ac1da35d49f3fd5a7b9de0f40a
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-21 11:48:41 +0600

    user info from jwt created

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { UserModel } from "../models/user.model.js";
+
+const isAuthenticated = async (req, res , next) => {
+    try {
+
+        const stra_cookie = req.cookies.Stra_cookie;
+
+        if (!stra_cookie) return next();
+
+        const decodedToken = jwt.verify( stra_cookie , process.env.JWT_SECRET_KEY );
+
+        const user = await UserModel.findById( decodedToken._id ).select( " -password");
+
+        req.userData = user;
+
+        next();
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export default isAuthenticated;
