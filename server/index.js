650d6d646092975b34a0af0d7b3ff585e1f651bc
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-21 11:25:23 +0600

    cookie added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { route } from './src/routes/user.routes.js';
 import dotenv from "dotenv";
 import connectDB from './src/db/db.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 3500;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3500;
 dotenv.config({path:"./.env"});
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.use("/api",route)
 
