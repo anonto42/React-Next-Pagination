eef15103e48f6805ca794ac571b46a3f1bd29cff
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-22 14:41:11 +0600

    bio added

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new Schema({
         type:String,
         required:true
     },
+    bio:{
+        type: String,
+        default:""
+    },
     avatar:{
         public_id:{
             type:String,
