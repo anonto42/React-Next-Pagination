c55086675fc1e4a92f6ed8e175db0f6a634ef4f9
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-22 14:41:56 +0600

    new group chat adding

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -1,3 +1,4 @@
+import { ALERT, REFETCH_CHATS } from "../constants/events.js";
 import { ChatModel } from "../models/chat.model.js";
 import { emitEvent } from "../utils/features.js";
 
@@ -8,7 +9,7 @@ export const newGroup = async ( req , res , next) => {
 
         if( member.length < 2 ) return next( new Error(`Group chat must have at least 2 members`,400));
 
-        const allMembers = [...member, req.userData];
+        const allMembers = [...member, req.userData._id];
 
         await ChatModel.create(
             {
@@ -19,7 +20,17 @@ export const newGroup = async ( req , res , next) => {
             }
         )
 
-        emitEvent(req)
+        emitEvent(req,ALERT,allMembers,`Welcome to ${name} group`);
+        emitEvent(req,REFETCH_CHATS,member);
+
+        return res
+        .status(201)
+        .json(
+            {
+                success: true,
+                message:"Group chat was created successfully"
+            }
+        )
         
     } catch (error) {
         console.log(error.message)
