5ca42376ba479cee3631ce4552348b26b9e147d3
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-24 13:11:12 +0600

    chat route added

--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -1,12 +1,18 @@
 import Exx from "express";
 import isAuthenticated from './../middlewares/auth.js';
-import { newGroup } from "../controllers/chat.controller.js";
+import { addMembers, myChat, myGroupChat, newGroup } from "../controllers/chat.controller.js";
 
 const chatRouter = Exx.Router();
 
 chatRouter.use(isAuthenticated)
 
 chatRouter.route("/new").post( newGroup )
+
+chatRouter.route("/my").post( myChat )
+
+chatRouter.route("/my/groups").post( myGroupChat )
+
+chatRouter.route("/add").post( addMembers )
 
 
 export default chatRouter;
