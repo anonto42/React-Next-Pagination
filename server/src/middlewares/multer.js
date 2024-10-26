41235978ce14da7875d7cc6f2f8e4c514077201f
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-26 09:58:41 +0600

    strage updated

--- server/src/middlewares/multer.js
+++ server/src/middlewares/multer.js
@@ -1,10 +1,20 @@
 import multer from "multer";
 
+
+const storage = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+            cb(null, "./public/temp");
+        },
+        filename: function (req, file, cb) {
+            cb(null,file.originalname);
+        }
+    }
+)
+
 export const multerUpload = multer(
     {
-        limits: {
-            fileSize: 1024 * 1024 * 5
-        }
+        storage
     }
 );
 
