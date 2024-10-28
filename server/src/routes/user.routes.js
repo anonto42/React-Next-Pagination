6f5fa9c00983fd0e83476571801c6e8d597ae3e2
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-10-29 05:08:26 +0600

    validator updated

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { getUserProfile, login, logout, newUser, serchUser } from "../controllers/user.controller.js";
 import { singleAvatarUpload } from "../middlewares/multer.js";
 import isAuthenticated from "../middlewares/auth.js";
-import { registerValidator, validate } from "../lib/validators.js";
 
 const route = express.Router();
 
@@ -10,7 +9,7 @@ route.route("/login").post( login )
 
 route.route("/logout").post( logout )
 
-route.route("/userCreate").post( singleAvatarUpload , registerValidator() , validate , newUser )
+route.route("/userCreate").post( singleAvatarUpload , newUser )
 
 route.route("/user").post( isAuthenticated , getUserProfile )
 
