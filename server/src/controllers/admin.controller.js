8733b793930ef41b966898081d55c5f6f294f8d5
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-11-15 10:03:15 +0600

    get all users

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,9 +1,20 @@
-
-
+import { UserModel } from "../models/user.model.js"
 
 
 export const allUsers = async (req, res, next) => {
     try {
+
+        const users = await UserModel.find(
+            {
+
+            }
+        )
+
+        return res
+        .status(200)
+        .json(
+            users
+        )
         
     } catch (error) {
         console.log(error)
