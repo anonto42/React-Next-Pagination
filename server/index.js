954a6c7200f4c7cb78f31399f9b3bcd6458d11a7
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-17 16:20:24 +0600

    socket on new message added

--- server/index.js
+++ server/index.js
@@ -6,9 +6,12 @@ import cookieParser from 'cookie-parser';
 import chatRouter from './src/routes/chat.routes.js';
 import AdminRoutes from './src/routes/admin.routes.js';
 import { Server } from 'socket.io';
+import { createServer } from "http"
+import { NEW_MESSAGE } from './src/constants/events.js';
 
 const app = express();
-const io = new Server(app,{});
+const server = createServer(app)
+const io = new Server(server,{});
 
 const port = process.env.PORT || 3500;
 
@@ -23,7 +26,20 @@ app.use("/api",route)
 app.use("/api/chat",chatRouter)
 app.use("/admin",AdminRoutes)
 
-app.listen( port , ()=> {
+// Socket routes started
+
+io.on("connection", (socket) => {
+    console.log("a user connected : " , socket.id);
+
+    socket.on(NEW_MESSAGE , () => {});
+
+
+    socket.on("disconnect", (socket) => {
+        console.log("user disconnected : " , socket.id);
+    });
+})
+
+server.listen( port , ()=> {
     connectDB();
     console.log("your server listening the port : " , port )
 })
