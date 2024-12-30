b1e21af25d48698af1e0f0f72a1ab1c5cf784fd5
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-31 00:56:17 +0600

    socket.io configed

--- server/index.js
+++ server/index.js
@@ -7,11 +7,15 @@ import chatRouter from './src/routes/chat.routes.js';
 import AdminRoutes from './src/routes/admin.routes.js';
 import { Server } from 'socket.io';
 import { createServer } from "http"
-import { NEW_MESSAGE } from './src/constants/events.js';
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from './src/constants/events.js';
+import { randomUUID as uuid } from 'crypto';
+import { getSockets } from './src/utils/features.js';
+import { MessageModel } from './src/models/message.model.js';
 
 const app = express();
 const server = createServer(app)
 const io = new Server(server,{});
+const userSocketIDs = new Map();
 
 const port = process.env.PORT || 3500;
 
@@ -28,14 +32,54 @@ app.use("/admin",AdminRoutes)
 
 // Socket routes started
 
+io.use((socket,next) => {
+    
+})
+
 io.on("connection", (socket) => {
-    console.log("a user connected : " , socket.id);
+    const user = {
+        name: "DJONGO",
+        _id:"askdka"
+    }
 
-    socket.on(NEW_MESSAGE , () => {});
+    userSocketIDs.set( user._id.toString(), socket.id);
 
+    console.log("a user connected : " , userSocketIDs );
+
+    socket.on(NEW_MESSAGE , async ({ chatId , members , messages}) => {
+        
+        const messageForRealTime = {
+            content: messages,
+            _id: uuid(),
+            sender:{
+             _id: user._id,
+             name: user.name
+            },
+            chat : chatId,
+            createdAt: new Date().toISOString()
+        };
+
+        const messageForDB = {
+            content: messages,
+            sender: user._id,
+            chat : chatId,
+        };
+
+        const usersSocket = getSockets(members)
+
+        io.to(usersSocket).emit(NEW_MESSAGE , {
+            chatId,
+            message: messageForRealTime
+        });
+        io.to(usersSocket).emit( NEW_MESSAGE_ALERT , { chatId })
+        console.log("New message : " , messageForRealTime);
+
+        await MessageModel.create(messageForDB)
+    });
 
     socket.on("disconnect", (socket) => {
         console.log("user disconnected : " , socket.id);
+        userSocketIDs.delete(user._id.toString());
     });
 })
 
@@ -43,3 +87,5 @@ server.listen( port , ()=> {
     connectDB();
     console.log("your server listening the port : " , port )
 })
+
+export { userSocketIDs }
