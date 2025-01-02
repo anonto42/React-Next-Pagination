3dbd4e3ff02a10e5e01ac2e4fa3ca00ca5fc8fc7
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-02 23:46:20 +0600

    added the Provider for the redux stor or the toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline } from '@mui/material';
 import { HelmetProvider } from'react-helmet-async';
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider >
-      <CssBaseline />
-      <App />
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider >
+        <CssBaseline />
+        <App />
+      </HelmetProvider>
+    </Provider>
   </StrictMode>,
 )
