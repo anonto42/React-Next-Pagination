5fcdaccc1858eb9aa71cb4361e1c90176ca42410
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-03 18:18:02 +0600

    and the stors user slice added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './reducer/auth';
+import userSlice from './reducer/user';
 
 const store = configureStore({
     reducer:{
-        [ authSlice.name ] : authSlice.reducer
+        [ authSlice.name ] : authSlice.reducer,
+        [ userSlice.name ] : userSlice.reducer
     }
 });
 
