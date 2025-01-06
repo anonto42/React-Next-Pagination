8fd3211b51c6b62c345d2aadf060a7066e668575
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-06 17:17:44 +0600

    gase64 uploaded

--- server/src/lib/helper.js
+++ server/src/lib/helper.js
@@ -3,3 +3,6 @@
 export const otherMembers = ( members , userId ) => {
     return members.find( ( member ) => member._id.toString() !== userId.toString() )
 }
+
+
+export const getBase64 = (file) => `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
