9ebfeab2fc992a4d834e7a08d5605acee94c7f9d
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-25 00:21:11 +0600

    created the call to action section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Arro from "@/assets/arrow-right.svg"
+import Image from 'next/image';
+import StarImag from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
+    <section className='bg-gradient-to-b from-white/60 to-[#d2ecff] py-24 px-5 xl:px-0 overflow-clip'>
+        <div className='container mx-auto'>
+            <div className='relative max-w-xl mx-auto'>
+                <h2 className='section-title'>Sign up for free today</h2>
+                <p className='section-discription mt-5'>Celebrate the joy of accomplishment with an app designd to track your progress and motivate your efforts.</p>
+                <Image 
+                    width={380}
+                    alt='star'
+                    src={StarImag}
+                    className='absolute -left-[350px] -top-[20px]'
+                />
+                <Image 
+                    width={380}
+                    alt='star'
+                    src={springImage}
+                    className='absolute -right-[331px] -top-[19px]'
+                />
+            </div>
+            <div className='flex gap-2 mt-10 justify-center'>
+                <button className='btn btn-primary'>Get for free</button>
+                <button className='btn btn-text gap-1'>
+                    <span>Learn more</span>
+                    <Image alt="arro" loading="lazy" src={Arro} className="h-5 w-5" />
+                </button>
+            </div>
+        </div>
+    </section>
   )
 }
 
