6a88e2956065be3795fd3a8e6c20f49dbeb513e5
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-24 21:29:49 +0600

    added the testmonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import LogoTicker from "@/components/LogoTicker";
 import Pricing from "@/components/Pricing";
 import ProductShowCase from "@/components/ProductShowCase";
+import Testmonial from "@/components/Testmonial";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <LogoTicker />
       <ProductShowCase />
       <Pricing />
+      <Testmonial />
     </div>
   );
 }
