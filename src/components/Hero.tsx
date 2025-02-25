e73feeb71bca6721b55b270f0c3c40b6828b41e0
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-25 19:14:05 +0600

    added the Hero motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+import React, { useRef } from 'react';
+import { motion , useScroll , useTransform } from 'framer-motion';
 import ArroIcon from "@/assets/arrow-right.svg";
 import Image from 'next/image';
 import cogImage from "@/assets/cog.png";
@@ -7,11 +9,22 @@ import noodleImage from "@/assets/noodle.png";
 
 
 const Hero = () => {
+    const herRef = useRef(null);
+    const { scrollYProgress } = useScroll(
+        {
+            target:herRef,
+            offset:["start end", "end start"]
+        }
+    );
+    const translateY = useTransform( scrollYProgress , [0 , 1], [150, -150]);
+    
   return (
-    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+    <section
+        ref={herRef}
+        className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
         <div className='container mx-auto px-5 xl:px-0'>
             <div className='md:flex items-center'>
-                <div className='md:w-[478px]'>
+                <div className='md:max-w-[500px]'>
                     <div className='tag'>Version 2.0 here</div>
                     <h1 className='text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text'>Pathway to productivity</h1>
                     <p className='text-xl text-[#010D3E] tracking-tight mt-6'>
@@ -33,25 +46,49 @@ const Hero = () => {
                         </button>
                     </div>
                 </div>
-                <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative'>
-                    <Image 
-                        src={cogImage} 
+                <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative lg:ml-40 xl:ml-80 2xl:ml-96'>
+                    <motion.img 
+                        src={cogImage.src} 
                         alt='Cog' 
                         className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0'
+                        animate={
+                            {
+                                translateY:[ -30 , 30]
+                            }
+                        }
+                        transition={
+                            {
+                                repeat: Infinity,
+                                repeatType: 'mirror',
+                                duration:3,
+                                ease:"easeInOut"
+                            }
+                        }
                     />
-                    <Image 
-                        src={cylenterImage} 
+                    <motion.img 
+                        src={cylenterImage.src} 
                         alt='image' 
                         width={220} 
                         height={220} 
                         className='hidden md:block -top-8 -left-32 md:absolute' 
+                        style={
+                            {
+                                translateY:translateY,
+                            }
+                        }
                     />
-                    <Image 
-                        src={noodleImage} 
+                    <motion.img 
+                        src={noodleImage.src} 
                         alt='image' 
                         width={220} 
                         height={220} 
                         className='hidden md:block top-[524] left-[448px] absolute rotate-[30deg]' 
+                        style={
+                            {
+                                translateY:translateY,
+                                rotate:30,
+                            }
+                        }
                     />
                 </div>
             </div>
