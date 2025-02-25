80d634174d1ddf39eedf7d9a6f760f42c2c43368
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-25 22:21:06 +0600

    added the hieight and the anemation

--- src/components/Testmonial.tsx
+++ src/components/Testmonial.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client"
+import React, { useRef } from 'react';
+import { motion , useScroll , useTransform } from 'framer-motion';
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -71,44 +73,64 @@ const firstColum = testimonials.slice(0,3);
 const secondColumn = testimonials.slice(3,6);
 const thirdColumn = testimonials.slice(6,9);
 
-const TestmonialsColum = ( props : { className?: string ; testmonial : typeof testimonials }) => (
-  <div className={`${props.className} flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]`}>
-    {
-      props.testmonial.map(({ text, imageSrc, name, username },index)=>(
-        <div key={index} className='card'>
-          <div>{text}</div>
-          <div className='flex items-center gap-2 mt-5'>
-            <Image 
-              src={imageSrc}
-              alt='name'
-              height={40}
-              width={40}
-              className='h-10 w-10 rounded-full'
-             />
-             <div className='flex flex-col'>
-               <div className='font-medium tracking-tight leading-5'>{name}</div>
-               <div className='leading-5 tracking-tight'>{username}</div>
-             </div>
-          </div>
-        </div>
-      ))
-    }
+const TestmonialsColum = ( props : { duration?:number ;className?: string ; testmonial : typeof testimonials }) => (
+  <div className={`${props.className}`}>
+    <motion.div
+      animate={
+        {
+          translateY:"-20%"
+        }
+      }
+      transition={
+        {
+          repeat: Infinity,
+          ease:"linear",
+          duration: props.duration || 10,
+          repeatType:"loop"
+        }
+      }
+      className='gap-6 flex flex-col pb-6' >
+      {[...new Array(2)].fill(0).map((_,index)=>(
+        <React.Fragment key={index} >
+          {
+            props.testmonial.map(({ text, imageSrc, name, username },index)=>(
+              <div key={index} className='card'>
+                <div>{text}</div>
+                <div className='flex items-center gap-2 mt-5'>
+                  <Image 
+                    src={imageSrc}
+                    alt='name'
+                    height={40}
+                    width={40}
+                    className='h-10 w-10 rounded-full'
+                  />
+                  <div className='flex flex-col'>
+                    <div className='font-medium tracking-tight leading-5'>{name}</div>
+                    <div className='leading-5 tracking-tight'>{username}</div>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </React.Fragment>
+      ))}
+    </motion.div>
   </div>
 )
 
 const Testmonial = () => {
   return (
     <section>
-      <div className='bg-white/60 rounded-md pb-10'>
+      <div className='bg-white/60 rounded-md'>
         <div className='flex justify-center'>
           <div className='tag mt-6 text-xl'>Testmonial</div>
         </div>
         <h2 className='section-title mt-5'>What our users say</h2>
         <p className='section-discription mt-5 max-w-2xl mx-auto'>From intuitive design to powerful features, out app has become an essentail tool for users around the world.</p>
-        <div className='flex justify-center gap-6'>
-          <TestmonialsColum testmonial={firstColum}/>
-          <TestmonialsColum testmonial={secondColumn} className='hidden md:flex'/>
-          <TestmonialsColum testmonial={thirdColumn} className='hidden lg:flex'/>
+        <div className='flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[700px] overflow-hidden'>
+          <TestmonialsColum testmonial={firstColum} duration={13}/>
+          <TestmonialsColum testmonial={secondColumn} className='hidden md:block' duration={15}/>
+          <TestmonialsColum testmonial={thirdColumn} className='hidden lg:block' duration={12}/>
         </div>
       </div>
     </section>
