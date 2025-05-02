0e41d3828a19491283f79bc334c3f137fc12d9e8
Sohidul Islam Ananto <anontom90@gmail.com>
2025-05-02 10:05:05 +0600

    new project init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const DMSans = DM_Sans({subsets: ["latin"]});
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${DMSans.className} antialiased bg-[#EFEEFE]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
